830057893e0a4795eb192d9526eb9a18d131136f
Walter Longo Neto <walterlongoneto@gmail.com>
2012-08-27 17:18:48 -0300

    Update app/code/local/RedFeet/Location/Block/Region.php
    Alterado para utilizar novos parametros de Region_Helper::getArray

--- app/code/local/RedFeet/Location/Block/Region.php
+++ app/code/local/RedFeet/Location/Block/Region.php
@@ -2,9 +2,11 @@
 
 class RedFeet_Location_Block_Region extends RedFeet_Location_Block_Abstract
 {
+    private $_country_id;
+    
     public function html() {
         $html = '';
-        if(count(Mage::helper('location/region')->getArray()) <= 1 || !is_integer($this->getValue())) {
+        if(count(Mage::helper('location/region')->getArray($this->getType(), $this->_country_id)) <= 1 || !is_integer($this->getValue())) {
             $this->addStyle('display', 'none;');
             $html.=$this->_getSelectHtml();
             $html.=$this->_getInputHtml();
@@ -27,7 +29,7 @@ class RedFeet_Location_Block_Region extends RedFeet_Location_Block_Abstract
         $html = '';
         $region_default_id = Mage::helper('location/region')->getDefaultId($this->getType(), $this->getValue());
                 
-        $array = Mage::helper('location/region')->getArray();
+        $array = Mage::helper('location/region')->getArray($this->getType(), $this->_country_id);
         
         foreach($array as $region_data) {
             $selected = ($region_data['value'] == $region_default_id) ? 'selected="selected"' : '';
@@ -44,5 +46,10 @@ class RedFeet_Location_Block_Region extends RedFeet_Location_Block_Abstract
     
     public function getClass() {
         return ($this->_class == '') ? 'location_region' : $this->_class.' location_region';
+    }
+    
+    public function setCountryId($country_id) {
+        $this->_country_id = $country_id;
+        return $this;
     }
 }
