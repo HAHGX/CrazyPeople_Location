5ba65150f58edc85574545de4a09f7ccbe917b81
Walter Longo Neto <walterlongoneto@gmail.com>
2012-08-28 09:13:07 -0300

    Update app/code/local/RedFeet/Location/Helper/Country.php
    Correção de bugs.

--- app/code/local/RedFeet/Location/Helper/Country.php
+++ app/code/local/RedFeet/Location/Helper/Country.php
@@ -9,8 +9,11 @@ class RedFeet_Location_Helper_Country extends Mage_Core_Helper_Abstract {
         return $options;        
     }
     
-    public function getDefaultId($type=null) {
+    public function getDefaultId($type=null, $value=null) {
+        if(!empty($value)) return $value;                
+        
         $default_country_id = Mage::getStoreConfig('general/country/default');
+                
         $customer = Mage::getSingleton('customer/session')->getCustomer();
         $addresses = array();
         $id = '';
@@ -25,13 +28,13 @@ class RedFeet_Location_Helper_Country extends Mage_Core_Helper_Abstract {
             $addresses['billing'] = $customer->getPrimaryBillingAddress();
             $addresses['shipping'] = $customer->getPrimaryShippingAddress();
         }
-        
+                
         switch($type) {
             case 'billing':
-                $id = (is_null($addresses['billing'])) ? $default_country_id : $addresses['billing']->getCountryId();
+                $id = (!is_object($addresses['billing'])) ? $default_country_id : $addresses['billing']->getCountryId();
                 break;                
             case 'shipping':
-                $id = (is_null($addresses['shipping'])) ? $default_country_id : $addresses['shipping']->getCountryId();
+                $id = (!is_object($addresses['shipping'])) ? $default_country_id : $addresses['shipping']->getCountryId();
                 break;                
             default:
                 $id = $default_country_id;
